74e6654365e10336e33f544e470dfc1bd0c97327
minji.hong <minji.hong@bespinglobal.com>
2020-04-14 13:50:27 +0900

    Deployed 2dd27c5 with MkDocs version: 1.1

--- production/services/comp/eip.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_eip" "eip" {
-  vpc         = true
-  depends_on  = [aws_internet_gateway.igw]
-
-  tags = {
-    Name        = "${var.resrc_prefix_nm}-eip"
-    Environment = var.environment
-  }
-}
